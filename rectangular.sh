2ae74420f9286490e59ccef03ce171ca49b97c0e
akashsagar78562 <akashsagar78562@gmail.com>
2022-03-10 07:52:41 +0530

    area of rectangle

--- rectangular.sh
+++ rectangular.sh
@@ -1,5 +1,8 @@
 read -p "Enter the width and height value of the rectangular " w h
-sqm=$(($w*$h))
+m=$(($w/3))
+m1=$(($h/3))
+
+sqm=$(($m*$m1))
 echo "Area of the rectangle is $sqm Square meter"
-acre=$(($sqm/4047))
-echo "Conversion from Square meter into acre " $acre
+#acre=$(($sqm/4047))
+#echo "Conversion from Square meter into acre " $acre
