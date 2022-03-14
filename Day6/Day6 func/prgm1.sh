2fb4515e38a2161afb41bf89c23a8330c69d1f21
akashsagar78562 <akashsagar78562@gmail.com>
2022-03-14 11:23:15 +0530

    Degree conversion

--- Day6/Day6 func/prgm1.sh
+++ Day6/Day6 func/prgm1.sh
@@ -1,9 +1,32 @@
 read -p "Press 1 to convert from frenhite to celcius or press 2 to convert from clecius to frenhite: " a
 
 tempconv() {
-	case i in
+	case $a in
 	1)
-		read -p "Enter Farenhite" f
-		if [[ $f>=32 && $f<=212 ]]
+		read -p "Enter Farenhite " f
+		if [[ $f -ge 32 && $f -le 212 ]]
 		then
-			t=$(echo
+			f=$(($f-32))
+			degc=$(($f*5/9))
+			echo "The celcius value is $degc"
+		else
+			echo "Please provide F value within 32-212"
+		fi
+		;;
+	2)
+		read -p "Enter Celcius " c
+		if [[ $c -ge 0 && $c -le 100 ]]
+		then
+			c=$(( ($c*9/5) +32))
+			echo "The farenhote value is $c"
+		else
+			echo "Please provide C value within 0-100"
+		fi
+		;;
+	*)
+		echo "Please select from the above option"
+		;;
+	esac
+}
+
+tempconv $a
